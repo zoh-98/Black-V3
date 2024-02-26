0246a72d8a66dbb20ba5cf292af7bd64545886be
Gry KJ <106838880+zoh-98@users.noreply.github.com>
2024-02-26 19:55:55 +0100

    Create Avatar.js

---
+++ BLACK/cmds/Avatar.js
@@ -0,0 +1,62 @@
+const Black = {
+    name: "افاتار",
+    Hide: false,
+    Owner: "Gry KJ",
+    Auth: 0,
+    Time: 3,
+    Info: "يجيب لك بروفايل شخص ما",
+    Class: "أدوات"
+  };
+  const cheerio = require('cheerio');
+const axios = require ("axios");
+module.exports = {
+  config: Black,
+  onType: async ({ api, event, args, black, usersData }) => {
+const tvm = args.join(" ");
+if (tvm == "حط" && event.senderID == config.OWNERID)
+{
+ if(["photo"].includes(event.messageReply.attachments[0].type))
+ {
+   api.changeAvatar(await funcs.str(event.messageReply.attachments[0].url), (err, data) => { if (err) return black.reply("حدث خطأ ف تغيير الصورة")})
+   
+ }
+ else return black.reply("رد على صورة");
+}
+if (event.messageReply)
+{
+  if (event.messageReply.senderID == config.OWNERID) return black.reply("لايمكنك اخد بروفايل سيدي زهير");
+   const avUrl = await usersdata.getAvatarUrl(event.messageReply.senderID)
+  black.reply({body: "┊ ┊ ⋆˚ ⁭تفضل ┊ ┊ ⋆˚ ⁭", attachment: await funcs.str(avUrl)})
+}
+if (Object.keys(event.mentions)[0] != undefined)
+{
+  if (Object.keys(event.mentions)[0] == config.OWNERID) return black.reply("لايمكنك اخد بروفايل سيدي زهير");
+  const avUrl = await usersdata.getAvatarUrl(Object.keys(event.mentions)[0])
+  black.reply({body: "┊ ┊ ⋆˚ ⁭تفضل ┊ ┊ ⋆˚ ⁭", attachment: await funcs.str(avUrl)})
+  
+}
+if (tvm.startsWith("https://facebook"))
+{
+  const res = await axios.get(tvm);
+
+const html = res.data;
+
+const $ = cheerio.load(html);
+
+const metaTags = {};
+$('meta').each((index, element) => {
+    const name = $(element).attr('name');
+    const content = $(element).attr('content');
+    if (name && content) {
+        metaTags[name] = content;
+    }
+});
+
+const UIDD = metaTags['apple-itunes-app'];
+const UID = UIDD?.split('//profile/')[1] || "";
+const avUrl = await usersdata.getAvatarUrl(UID);
+if (!UID) return black.reply("حذث خطأ");
+black.reply({body: "┊ ┊ ⋆˚ ⁭تفضل ┊ ┊ ⋆˚ ⁭", attachment: await funcs.str(avUrl)})
+}
+}
+   };
