0a24aa90ca1294d2b05e23c8f44269f7e0fecc1a
Gry KJ <106838880+zoh-98@users.noreply.github.com>
2024-02-26 20:20:01 +0100

    Update Avatar.js

--- BLACK/cmds/Avatar.js
+++ BLACK/cmds/Avatar.js
@@ -18,20 +18,20 @@ if (tvm == "حط" && event.senderID == config.OWNERID)
  if(["photo"].includes(event.messageReply.attachments[0].type))
  {
    api.changeAvatar(await funcs.str(event.messageReply.attachments[0].url), (err, data) => { if (err) return black.reply("حدث خطأ ف تغيير الصورة")})
-   
+   black.reply("تم!")     
  }
  else return black.reply("رد على صورة");
 }
 if (event.messageReply)
 {
   if (event.messageReply.senderID == config.OWNERID) return black.reply("لايمكنك اخد بروفايل سيدي زهير");
-   const avUrl = await usersdata.getAvatarUrl(event.messageReply.senderID)
+   const avUrl = await usersData.getAvatarUrl(event.messageReply.senderID)
   black.reply({body: "┊ ┊ ⋆˚ ⁭تفضل ┊ ┊ ⋆˚ ⁭", attachment: await funcs.str(avUrl)})
 }
 if (Object.keys(event.mentions)[0] != undefined)
 {
   if (Object.keys(event.mentions)[0] == config.OWNERID) return black.reply("لايمكنك اخد بروفايل سيدي زهير");
-  const avUrl = await usersdata.getAvatarUrl(Object.keys(event.mentions)[0])
+  const avUrl = await usersData.getAvatarUrl(Object.keys(event.mentions)[0])
   black.reply({body: "┊ ┊ ⋆˚ ⁭تفضل ┊ ┊ ⋆˚ ⁭", attachment: await funcs.str(avUrl)})
   
 }
@@ -54,9 +54,9 @@ $('meta').each((index, element) => {
 
 const UIDD = metaTags['apple-itunes-app'];
 const UID = UIDD?.split('//profile/')[1] || "";
-const avUrl = await usersdata.getAvatarUrl(UID);
+const avUrl = await usersData.getAvatarUrl(UID);
 if (!UID) return black.reply("حذث خطأ");
-black.reply({body: "┊ ┊ ⋆˚ ⁭تفضل ┊ ┊ ⋆˚ ⁭", attachment: await funcs.str(avUrl)})
+black.reply({body: "┊ ┊ ⋆˚ ⁭تفضل ┊ ┊ ⋆˚ ⁭",attachment: await funcs.str(avUrl)})
 }
 }
    };
